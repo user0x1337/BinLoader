e3e37a8e2ecc00a4ed635c3b67098c56e4035cf1
user0x1337 <user0x1337@protonmail.com>
2022-07-07 16:29:54 +0200

    init commit

---
+++ binLoader.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+#include "inc/loader.h"
+
+int main(int argc, char *argv[]) {
+    fprintf(stdout, "Blub\n");
+
+
+    return 0;
+}
---
+++ inc/loader.cpp
@@ -0,0 +1,54 @@
+#include <bfd.h>
+#include "loader.h"
+
+void unload_binary(Binary *bin) {
+    size_t i;
+    Section *sec;
+
+    for(i = 0; i < bin->sections.size(); i++) {
+        sec = &bin->sections[i];
+        if (sec->bytes) {
+            free(sec->bytes);
+        }
+    }
+}
+
+static bfd* open_bfd(std::string &fname) {
+    static int bfd_inited = 0;
+    bfd *bfd_h;
+
+    if (!bfd_inited) {
+        bfd_init();
+        bfd_inited = 1;
+    }
+
+    bfd_h = bfd_openr(fname.c_str(), NULL);
+    if (!bfd_h) {
+        fprintf(stderr, "failed to open binary '%s' (%s)\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+
+    if (!bfd_check_format(bfd_h, bfd_object)) {
+        fprintf(stderr, "file '%s' does not look like an executable (%s)\n", fname.c_str(),
+                bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+
+    bfd_set_error(bfd_error_no_error);
+    if (bfd_get_flavour(bfd_h) == bfd_target_unknown_flavour) {
+        fprintf(stderr, "unrecognized format for binary '%s' (%s)\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+
+    return bfd_h;
+}
+
+static int load_binary_bfd(std::string &fname, Binary *bin, Binary::BinaryType type) {
+    int ret;
+    bfd *bfd_h;
+    const bfd_arch_info_type  *bfd_info;
+}
+
+int load_binary(std::string &fname, Binary *bin, Binary::BinaryType type) {
+    return load_binary_bfd(fname, bin, type);
+}
